09e559eb1681d5fd6b2ebf65bbfa823bab2bbb7c
David Wood <david.wood@huawei.com>
2021-11-08 09:18:55 +0000

    load dwarf objects to find split compilation units
    Load the dwarf object from the path constructed by
    `dwo_id_and_path_of_die` and find the compilation unit with the
    expected `DwoId`.
    
    Signed-off-by: David Wood <david.wood@huawei.com>

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 use thiserror::Error;
-use tracing::trace;
+use tracing::{span, trace, Level};
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 use typed_arena::Arena;
 
 #[derive(Debug, Error)]
@@ -25,10 +26,11 @@ struct Opt {
 }
 
 /// Load and parse an object file.
-fn load_object_file<'a>(
-    arena_mmap: &'a Arena<memmap::Mmap>,
-    path: &'a Path,
-) -> Result<object::File<'a>> {
+#[tracing::instrument(level = "trace", skip(arena_mmap))]
+fn load_object_file<'input, 'arena>(
+    arena_mmap: &'arena Arena<memmap::Mmap>,
+    path: &'input Path,
+) -> Result<object::File<'arena>> {
     let file = fs::File::open(&path)
         .with_context(|| format!("failed to open object file at {}", path.display()))?;
 
@@ -51,13 +53,12 @@ fn runtime_endian_of_object<'a>(obj: &object::File<'a>) -> gimli::RunTimeEndian 
 
 /// Loads a section of a file from `object::File` into a `gimli::EndianSlice`. Expected to be
 /// curried using a closure and provided to `Dwarf::load`.
-fn load_file_section<'input, 'arena, Endian: gimli::Endianity>(
+fn load_file_section<'input, 'arena>(
     id: gimli::SectionId,
     file: &object::File<'input>,
     is_dwo: bool,
-    endian: Endian,
     arena_data: &'arena Arena<Cow<'input, [u8]>>,
-) -> Result<gimli::EndianSlice<'arena, Endian>> {
+) -> Result<gimli::EndianSlice<'arena, gimli::RunTimeEndian>> {
     let name = if is_dwo {
         id.dwo_name()
     } else {
@@ -70,7 +71,10 @@ fn load_file_section<'input, 'arena, Endian: gimli::Endianity>(
         None => Cow::Owned(Vec::with_capacity(1)),
     };
     let data_ref = (*arena_data.alloc(data)).borrow();
-    Ok(gimli::EndianSlice::new(data_ref, endian))
+    Ok(gimli::EndianSlice::new(
+        data_ref,
+        runtime_endian_of_object(file),
+    ))
 }
 
 /// Returns the `DwoId` of a DIE.
@@ -143,38 +147,103 @@ fn dwo_id_and_path_of_die<R: gimli::Reader>(
     Ok(Some((dwo_id, dwo_path)))
 }
 
-fn main() -> Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::TRACE)
-        .init();
+/// Helper enum returned by the operation passed to `with_each_die` so that the closure can
+/// manage the control flow of the loop in the calling function.
+enum WithEachControlFlow {
+    /// `continue`
+    Continue,
+    /// `break`
+    #[allow(dead_code)]
+    Break,
+}
 
-    let opt = Opt::from_args();
-    trace!("{:?}", opt);
-
-    let arena_mmap = Arena::new();
-    let obj = load_object_file(&arena_mmap, &opt.executable)?;
-
-    let arena_data = Arena::new();
-    let endian = runtime_endian_of_object(&obj);
-    let mut load_section = |id: gimli::SectionId| -> Result<_, _> {
-        load_file_section(id, &obj, false, endian, &arena_data)
-    };
-
-    let dwarf = gimli::Dwarf::load(&mut load_section)?;
+/// Iterate over each debug information entry in an object, invoking a closure to perform an
+/// operation on each.
+fn with_each_die<Reader, Op>(dwarf: &gimli::Dwarf<Reader>, mut op: Op) -> Result<()>
+where
+    Reader: gimli::Reader,
+    Op: FnMut(
+        &gimli::Dwarf<Reader>,
+        &gimli::Unit<Reader>,
+        &gimli::DebuggingInformationEntry<Reader>,
+    ) -> Result<WithEachControlFlow>,
+{
     let mut iter = dwarf.units();
     while let Some(header) = iter.next()? {
         let unit = dwarf.unit(header)?;
         let abbreviations = dwarf.abbreviations(&unit.header)?;
         let mut entry_cursor = unit.header.entries(&abbreviations);
         while let Some((_, die)) = entry_cursor.next_dfs()? {
+            match op(&dwarf, &unit, &die)? {
+                WithEachControlFlow::Continue => continue,
+                WithEachControlFlow::Break => break,
+            }
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .with_env_filter(EnvFilter::from_env("RUST_DWP_LOG"))
+        .init();
+
+    let opt = Opt::from_args();
+    trace!(?opt);
+
+    let arena_mmap = Arena::new();
+    let obj = load_object_file(&arena_mmap, &opt.executable)?;
+
+    let arena_data = Arena::new();
+    let mut load_section =
+        |id: gimli::SectionId| -> Result<_, _> { load_file_section(id, &obj, false, &arena_data) };
+
+    let mut dwos = Vec::new();
+
+    {
+        let _guard = span!(Level::TRACE, "read dwarf objects from executable");
+        let exec_dwarf = gimli::Dwarf::load(&mut load_section)?;
+        with_each_die(&exec_dwarf, |dwarf, unit, die| {
             if die.tag() != gimli::DW_TAG_compile_unit {
-                continue;
+                return Ok(WithEachControlFlow::Continue);
             }
 
             if let Some((dwo_id, path)) = dwo_id_and_path_of_die(&dwarf, &unit, &die)? {
                 println!("dwo id: {:#x}, dwo path: {:?}", dwo_id.0, path);
+                dwos.push((dwo_id, path));
             }
-        }
+
+            Ok(WithEachControlFlow::Continue)
+        })?;
+    }
+
+    for (target_dwo_id, path) in dwos {
+        let _guard = span!(
+            Level::TRACE,
+            "find compilation unit in dwarf object",
+            dwo_id = target_dwo_id.0,
+            path = path.to_str().unwrap()
+        );
+        let dwo_obj = load_object_file(&arena_mmap, &path)?;
+        let mut load_dwo_section = |id: gimli::SectionId| -> Result<_, _> {
+            load_file_section(id, &dwo_obj, true, &arena_data)
+        };
+
+        let dwo_dwarf = gimli::Dwarf::load(&mut load_dwo_section)?;
+        with_each_die(&dwo_dwarf, |_dwarf, unit, die| {
+            if die.tag() != gimli::DW_TAG_compile_unit {
+                return Ok(WithEachControlFlow::Continue);
+            }
+
+            if let Some(dwo_id) = dwo_id_of_die(&unit, &die)? {
+                if target_dwo_id == dwo_id {
+                    println!("found dwo id: {:#x}", dwo_id.0);
+                }
+            }
+
+            Ok(WithEachControlFlow::Continue)
+        })?;
     }
 
     Ok(())
