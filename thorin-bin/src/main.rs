ae0c4f4fe2b9c55148230cb1bb4f4aff63712a6e
David Wood <david.wood@huawei.com>
2021-12-07 09:12:51 +0000

    introduce abstraction over arenas
    To prevent `thorin` from depending on `typed_arena` and `memmap2` (e.g.
    rustc has its own versions of these that can be used), introduce a
    `Session` trait that can be implemented by the user of `thorin` to
    implement these functions in an appropriate way.
    
    Signed-off-by: David Wood <david.wood@huawei.com>

--- thorin-bin/src/main.rs
+++ thorin-bin/src/main.rs
@@ -1,8 +1,13 @@
+use memmap2::Mmap;
+use std::borrow::Borrow;
+use std::fs::File;
+use std::path::Path;
 use std::{io::stderr, path::PathBuf};
 use structopt::StructOpt;
 use tracing::trace;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use tracing_tree::HierarchicalLayer;
+use typed_arena::Arena;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "thorin", about = "merge dwarf objects into dwarf packages")]
@@ -18,6 +23,36 @@ struct Opt {
     output: PathBuf,
 }
 
+/// Implementation of `thorin::Session` using `typed_arena` and `memmap2`.
+#[derive(Default)]
+struct Session<Relocations> {
+    arena_data: Arena<Vec<u8>>,
+    arena_mmap: Arena<Mmap>,
+    arena_relocations: Arena<Relocations>,
+}
+
+impl<Relocations> Session<Relocations> {
+    fn alloc_mmap<'arena>(&'arena self, data: Mmap) -> &'arena Mmap {
+        (*self.arena_mmap.alloc(data)).borrow()
+    }
+}
+
+impl<Relocations> thorin::Session<Relocations> for Session<Relocations> {
+    fn alloc_data<'arena>(&'arena self, data: Vec<u8>) -> &'arena [u8] {
+        (*self.arena_data.alloc(data)).borrow()
+    }
+
+    fn alloc_relocation<'arena>(&'arena self, data: Relocations) -> &'arena Relocations {
+        (*self.arena_relocations.alloc(data)).borrow()
+    }
+
+    fn read_input<'arena>(&'arena self, path: &Path) -> std::io::Result<&'arena [u8]> {
+        let file = File::open(&path)?;
+        let mmap = (unsafe { Mmap::map(&file) })?;
+        Ok(self.alloc_mmap(mmap))
+    }
+}
+
 fn main() -> Result<(), thorin::DwpError> {
     let subscriber = Registry::default().with(EnvFilter::from_env("RUST_DWP_LOG")).with(
         HierarchicalLayer::default()
@@ -31,5 +66,6 @@ fn main() -> Result<(), thorin::DwpError> {
     let opt = Opt::from_args();
     trace!(?opt);
 
-    thorin::package(opt.inputs, opt.executables, opt.output)
+    let sess = Session::default();
+    thorin::package(&sess, opt.inputs, opt.executables, opt.output)
 }
