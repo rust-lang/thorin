732a503cec65ff7f4d2843b71018886278200274
Kyle Huey <khuey@kylehuey.com>
2026-06-19 06:58:43 -0700

    Support GCing DWPs.

---
+++ tests/inputs/gc-multi-dwp-v4-b-dwo.s
@@ -0,0 +1,108 @@
+# Second `.dwo` for gc-multi-dwp-v4.test (DWARF4 GNU extension).
+#
+# Structurally similar to gc-loclist-v4-dwo.s, but with DWO ID 0xde and a longer
+# leading subprogram name so that the `target` DIE sits at a DIFFERENT
+# CU-relative offset. The DW_OP_call4 offset in its location list therefore
+# differs from the other CU's, so if one CU's offset remap bled into the other
+# the patched call offset would be visibly wrong.
+#
+#   compile_unit (children, DW_AT_GNU_dwo_id = 0xde)
+#     subprogram "dead_function_b": GNU_addr_index 0 (tombstoned -> dead)
+#     subprogram "live_func_b": GNU_addr_index 1 (live), has children
+#       variable "v": DW_AT_location = sec_offset -> loclist in .debug_loc.dwo
+#     subprogram "target_b": GNU_addr_index 2 (tombstoned, kept alive by the
+#                            DW_OP_call4 in the location list expression)
+#
+# DWO ID is 0xde.
+
+	.section	.debug_info.dwo,"e",@progbits
+	.long	.Ldebug_info_dwo_end-.Ldebug_info_dwo_start # Length of Unit
+.Ldebug_info_dwo_start:
+	.short	4                               # DWARF version number
+	.long	0                               # Offset Into Abbrev. Section
+	.byte	8                               # Address Size
+
+	# Abbrev 1: DW_TAG_compile_unit (has children)
+	.byte	1
+	.quad	0xde                            # DW_AT_GNU_dwo_id
+
+	# Abbrev 2: DW_TAG_subprogram "dead_function_b" (GNU_addr_index 0, tombstoned -> dead)
+	.byte	2
+	.byte	0                               # DW_AT_low_pc: GNU_addr_index index 0
+	.asciz	"dead_function_b"               # DW_AT_name (deliberately long)
+
+	# Abbrev 3: DW_TAG_subprogram "live_func_b" (GNU_addr_index 1, live, has children)
+	.byte	3
+	.byte	1                               # DW_AT_low_pc: GNU_addr_index index 1
+	.asciz	"live_func_b"                   # DW_AT_name
+
+	# Abbrev 4: DW_TAG_variable "v" (DW_AT_location = sec_offset -> loclist)
+	.byte	4
+	.long	0                               # DW_AT_location: sec_offset (byte 0 of .debug_loc.dwo)
+
+	# End of live_func_b children
+	.byte	0
+
+	# Abbrev 2: DW_TAG_subprogram "target_b" (GNU_addr_index 2, tombstoned, kept alive by loclist ref)
+.Ltarget:
+	.byte	2
+	.byte	2                               # DW_AT_low_pc: GNU_addr_index index 2
+	.asciz	"target_b"                      # DW_AT_name
+
+	.byte	0                               # End Of Children Mark (compile_unit)
+.Ldebug_info_dwo_end:
+
+	.section	.debug_loc.dwo,"e",@progbits
+	# GNU split DWARF uses DW_LLE entry types even in DWARF4.
+	# DW_LLE_startx_length
+	.byte	0x03                            # DW_LLE_startx_length
+	.byte	1                               # begin: GNU_addr_index 1 (ULEB128)
+	.long	0x10                            # length (4 bytes)
+	.short	5                               # Expression length (2 bytes)
+	.byte	0x99                            # DW_OP_call4
+	.long	.Ltarget-.Ldebug_info_dwo_start+4 # 4-byte CU-relative offset of target_b
+	# DW_LLE_end_of_list
+	.byte	0x00
+
+	.section	.debug_abbrev.dwo,"e",@progbits
+	# Abbrev 1: DW_TAG_compile_unit, has children, DW_AT_GNU_dwo_id(data8)
+	.byte	1
+	.byte	17                              # DW_TAG_compile_unit
+	.byte	1                               # DW_CHILDREN_yes
+	.uleb128	0x2131                  # DW_AT_GNU_dwo_id
+	.uleb128	7                       # DW_FORM_data8
+	.byte	0
+	.byte	0
+
+	# Abbrev 2: DW_TAG_subprogram, no children, DW_AT_low_pc(GNU_addr_index) + DW_AT_name(string)
+	.byte	2
+	.byte	46                              # DW_TAG_subprogram
+	.byte	0                               # DW_CHILDREN_no
+	.byte	17                              # DW_AT_low_pc
+	.uleb128	0x1f01                  # DW_FORM_GNU_addr_index
+	.byte	3                               # DW_AT_name
+	.byte	8                               # DW_FORM_string
+	.byte	0
+	.byte	0
+
+	# Abbrev 3: DW_TAG_subprogram, has children, DW_AT_low_pc(GNU_addr_index) + DW_AT_name(string)
+	.byte	3
+	.byte	46                              # DW_TAG_subprogram
+	.byte	1                               # DW_CHILDREN_yes
+	.byte	17                              # DW_AT_low_pc
+	.uleb128	0x1f01                  # DW_FORM_GNU_addr_index
+	.byte	3                               # DW_AT_name
+	.byte	8                               # DW_FORM_string
+	.byte	0
+	.byte	0
+
+	# Abbrev 4: DW_TAG_variable, no children, DW_AT_location(sec_offset)
+	.byte	4
+	.byte	52                              # DW_TAG_variable
+	.byte	0                               # DW_CHILDREN_no
+	.byte	2                               # DW_AT_location
+	.byte	23                              # DW_FORM_sec_offset
+	.byte	0
+	.byte	0
+
+	.byte	0                               # End of abbreviations
